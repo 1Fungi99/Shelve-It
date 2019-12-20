79bfd3c327d8d57a3124a36447c003f60c7eda14
Hibah Shafi <hibahshafi.hs@gmail.com>
2019-12-19 21:07:08 -0600

    one HTML route is working for homepage

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,9 @@ var db = require("../models");
 
 module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/user", function (req, res) {
+    db.User.findAll({}).then(function (data) {
+      res.json(data);
     });
   });
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,18 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.User.findAll({}).then(function (data) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        msg: "Hello"
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +20,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
