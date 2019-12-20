8dc41b36c39580e5de70936a7cb260938a4e5046
Hibah Shafi <hibahshafi.hs@gmail.com>
2019-12-19 20:40:26 -0600

    Added the story.js model, added the Db config folder, modfied the user.js file, updated the index.js so need everyone to add a .env file

---
+++ models/story.js
@@ -0,0 +1,62 @@
+
+module.exports = function (sequelize, DataTypes) {
+  var Story = sequelize.define("Story", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    story: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    draft: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      validate: {
+        len: [1]
+      }
+    },
+    submitted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    catergory: {
+      type: DataTypes.STRING,
+      defaultValue: "Other"
+    },
+    storyType: {
+      type: DataTypes.STRING,
+      defaultValue: "Other"
+    },
+    language: {
+      type: DataTypes.STRING,
+      defaultValue: "English"
+    },
+    author: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+
+  }, {
+    freezeTableName: true // Model tableName will be the same as the model name
+  }
+
+  );
+
+  Story.associate = function (models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Story.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Story;
+};
