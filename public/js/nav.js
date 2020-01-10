a0a82e7f4c94d00ce649d4624bdb136a613f9a28
Justin Cheng <ijustin17@gmail.com>
2020-01-09 19:25:46 -0600

    database work for users

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,15 +1,24 @@
+import { application } from "express";
+
+// Dependancies
+// Grabbing the new User model
+// var ajax = require("ajax");
+
+// ================= Reset Modal Logic Below =================
+// Once modal is closed, all fields reset.
 $("#nav_login").on("click", function() {
   var loginForm = document.getElementById("login-form");
   loginForm.reset();
   $("#log-in").modal("show");
 });
-
 $("#nav_signup").on("click", function() {
   var signupForm = document.getElementById("signup-form");
   signupForm.reset();
   $("#sign-up").modal("show");
 });
+// ================= Reset Modal Logic Above =================
 
+// ================= Show Password Logic Below =================
 $("#signup-show").on("click", function() {
   if ($("#password")[0].type == "password") {
     $("#password")[0].type = "text";
@@ -17,7 +26,6 @@ $("#signup-show").on("click", function() {
     $("#password")[0].type = "password";
   }
 });
-
 $("#login-show").on("click", function() {
   if ($("#password")[0].type == "password") {
     $("#password")[0].type = "text";
@@ -25,6 +33,37 @@ $("#login-show").on("click", function() {
     $("#password")[0].type = "password";
   }
 });
+// ================= Show Password Logic Above =================
+
+$("#login-submit").on("click", function() {
+  $;
+});
+
+$("#signup-submit").on("click", function() {
+  var newUserData = {
+    username: $("#username").val(),
+    pass: $("#password").val(),
+    first_name: $("#first-name").val(),
+    middle_initial: $("#middle_initial").val(),
+    last_name: $("#last-name").val(),
+    email_address: $("#email").val()
+  };
+  if ($("#passowrd").text() === $("#passowrd-confirm")) {
+    $.ajax({
+      type : "POST",
+      contentType : "application/json",
+      url : window.location + "api/users/save",
+      data : JSON.stringify(formData),
+      dataType : 'json',
+      success : function(customer) {
+        $("#postResultDiv").html("<p>" + 
+          "Post Successfully! <br>" +
+          "--> " + newUserData.first_name + " " + " " + newUserData.middle_initial+ " "+ newUserData.last_name + " " + newUserData.username+ " " + newUserData.password+ " " + newUserData.email_address + ", createdAt: " + customer.createdAt+  "</p>"); 
+      },
+  })
+};
+
+// ================= Google Signup/Login Logic Below =================
 
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
@@ -40,3 +79,4 @@ function onSignIn(googleUser) {
   var id_token = googleUser.getAuthResponse().id_token;
   console.log("ID Token: " + id_token);
 }
+// ================= Google Signup/Login Logic Above =================
