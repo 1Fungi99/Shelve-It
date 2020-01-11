3ec383b40f2b46d4849c2153dff3e9240fb38405
Jhayla Young <jhayla.young@gmail.com>
2020-01-11 14:24:02 -0600

    added transition stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,10 @@
 //ON Load//
 
+var loader = document.getElementById("loader");
+
+window.addEventListener("load", function () {
+  // loader.style.height = "100px";
+  // loader.style.width = "100px";
+  // loader.style.borderRadius = "50%";
+  // loader.style.visibility = "hidden"
+});
