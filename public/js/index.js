04e1ad2fafd9c32e5fcc6bbb0895d03506e5d820
Jhayla Young <jhayla.young@gmail.com>
2020-01-14 08:27:56 -0600

    kinda working transition, need help

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,8 @@
-//ON Load//
+// //ON Load//
 
 var loader = document.getElementById("loader");
 
 window.addEventListener("load", function () {
-  // loader.style.height = "100px";
-  // loader.style.width = "100px";
-  // loader.style.borderRadius = "50%";
-  // loader.style.visibility = "hidden"
+  loader.style.height = "1500px";
+  loader.style.width = "1500px";
 });
