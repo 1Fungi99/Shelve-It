c53ae81d1e6b7888615b59129afbe2817a19d5fd
Jhayla Young <jhayla.young@gmail.com>
2020-01-14 18:15:41 -0600

    finished loader

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,14 @@
 // //ON Load//
 
 var loader = document.getElementById("loader");
+setTimeout(function () { document.getElementById("loader").style.display = "none"; }, 7000);
 
-window.addEventListener("load", function () {
-  loader.style.height = "1500px";
-  loader.style.width = "1500px";
-});
+// document.querySelector("#loader").addEventListener("load", function () {
+//   loader.style.height = "1500px";
+//   loader.style.width = "1500px";
+//   document.getElementById("loader").style.display = "none";
+
+//   document.querySelector("#loader").remove();
+
+// });
+
