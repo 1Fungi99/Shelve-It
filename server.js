020a5eac6eab4ff94bd51843189d33d58610cf99
Hibah Shafi <hibahshafi.hs@gmail.com>
2020-01-16 20:48:23 -0600

    Revert "Revert "Revert "Revert "Updated the Sync flag to True""""
    This reverts commit 4aa1c0abe9f134585d12419aee0f44fb0bbd261f.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
